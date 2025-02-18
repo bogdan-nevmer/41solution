d636a4ad0adb1432ade4ac009b9f48bcfec150f4
bogdan nevmer <weckman689@gmail.com>
2025-02-18 18:54:04 +0300

    update

---
+++ 3/main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#define N 5
+
+
+#define MAX 100
+using namespace std;
+
+
+int sum_main_elements(int maxtrix[N][N]) {}
+int sum_add_element(int maxtrix[N][N]) {
+
+	int sum = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+
+	}
+}
+
+int main() {
+
+
+
+	int matrix[N][N]{};
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			matrix[i][j] = rand() % MAX;
+		}
+		cout << endl;
+	}
+
+	cout << sum_main_elements(matrix);
+	cout << sum_add_elements(matrix);
+	return 0;
+}
